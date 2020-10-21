3304199cb6a54e1cc178f5341ae665ea9013c152
ai-small <aismall@yandex.ru>
2020-10-22 09:08:26 +1200

    added some styles and js for signin and signup popups

--- src/js/script.js
+++ src/js/script.js
@@ -1,89 +1,78 @@
 // eslint-disable-next-line wrap-iife
 (function () {
+  const body = document.body;
   const openMobileMenu = document.querySelector('.button__icon_burger');
-  const closeMobileMenu = document.querySelector('.button__icon_close');
+  const closeMobileMenuButton = document.querySelector('.button__icon_close');
+  const authButton = document.querySelector('.navigation__button');
+
+  const overlay = document.querySelector('.overlay');
+  const navigation = document.querySelector('.navigation');
+  const headerMenu = document.querySelector('.header__menu');
+
+  const popupSignIn = document.getElementById('popup-signin');
+  const popupSignUp = document.getElementById('popup-signup');
+  const popupCloseButtons = document.getElementsByClassName('popup__mobile-button');
+  const signUpButton = document.getElementById('open-signup');
+  const signInButton = document.getElementById('open-signin');
+
+  function closeMobileMenu() {
+    if (!navigation.classList.contains('hidden')) {
+      navigation.classList.add('hidden');
+      navigation.classList.remove('navigation_flex');
+      headerMenu.classList.remove('header__menu_theme_dark');
+      overlay.classList.add('hidden');
+      openMobileMenu.classList.remove('hidden');
+      closeMobileMenuButton.classList.add('hidden');
+      console.log('close burger!');
+    }
+  }
+
+  function addListenerToCloseButton(closePopupButton) {
+    closePopupButton.addEventListener('click', function() {
+      closePopupButton.closest('.popup').classList.add('hidden');
+      openMobileMenu.classList.remove('hidden');
+      body.classList.remove('overflow-hidden');
+    });
+  }
 
   // eslint-disable-next-line prefer-arrow-callback
   openMobileMenu.addEventListener('click', function() {
-    const overlay = document.querySelector('.overlay');
-    const navigationHidden = document.querySelector('.navigation');
-    const headerMenu = document.querySelector('.header__menu');
-
-    if (navigationHidden.classList.contains('hidden')) {
-      navigationHidden.classList.remove('hidden');
-      navigationHidden.classList.add('navigation_flex');
+    if (navigation.classList.contains('hidden')) {
+      navigation.classList.remove('hidden');
+      navigation.classList.add('navigation_flex');
       headerMenu.classList.add('header__menu_theme_dark');
       overlay.classList.remove('hidden');
       openMobileMenu.classList.add('hidden');
-      closeMobileMenu.classList.remove('hidden');
+      closeMobileMenuButton.classList.remove('hidden');
       console.log('remove hidden, set theme_dark, set overlay to page');
     }
   });
 
-  closeMobileMenu.addEventListener('click', function() {
-    console.log('close!');
-    const overlay = document.querySelector('.overlay');
-    const navigationHidden = document.querySelector('.navigation');
-    const headerMenu = document.querySelector('.header__menu');
-    if (!navigationHidden.classList.contains('hidden')) {
-      navigationHidden.classList.add('hidden');
-      navigationHidden.classList.remove('navigation_flex');
-      headerMenu.classList.remove('header__menu_theme_dark');
-      overlay.classList.add('hidden');
-      openMobileMenu.classList.remove('hidden');
-      closeMobileMenu.classList.add('hidden');
-    }
+  closeMobileMenuButton.addEventListener('click', closeMobileMenu);
+
+  authButton.addEventListener('click', function() {
+    const closePopupButton = popupCloseButtons.namedItem('signin-close');
+    closeMobileMenu();
+    openMobileMenu.classList.add('hidden');
+    popupSignIn.classList.remove('hidden');
+    body.classList.add('overflow-hidden');
+
+    addListenerToCloseButton(closePopupButton);
   });
-  // const cardsContainer = document.querySelector('.places-list');
-  // const containerPopupContent = document.querySelector('.popup__content');
 
-  // const openAddCardButton = document.querySelector('.user-info__button_type_new');
-  // const openEditButton = document.querySelector('.user-info__button_type_edit');
+  signUpButton.addEventListener('click', function() {
+    popupSignIn.classList.add('hidden');
+    popupSignUp.classList.remove('hidden');
+    const closePopupButton = popupCloseButtons.namedItem('signup-close');
+    closePopupButton.classList.remove('hidden');
+    addListenerToCloseButton(closePopupButton);
+  });
 
-  // const avatarContainer = document.querySelector('.user-info__photo');
-
-  // const createCard = (...arg) => new Card(...arg);
-  // const createImagePopup = (...arg) => new ImagePopup(...arg);
-  // const createFormValidator = (...arg) => new FormValidator(...arg);
-  // const createUserInfo = (...arg) => new UserInfo(...arg);
-  // const getInitialCards = () => {
-
-  //   api.getCards()
-  //     .then(res => {
-  //       cardList.render(res, userInfo)
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     })
-
-  // }
-
-  // const api = new Api(config);
-  // const cardList = new CardList(cardsContainer, api, createCard, createImagePopup);
-  // const placeForm = new NewPlacePopup(containerPopupContent, createFormValidator, cardList._addCard, createImagePopup, api);
-  // const editProfileForm = new EditProfilePopup(containerPopupContent, createUserInfo, createFormValidator, api, openEditButton, avatarContainer);
-  // const updateAvatarPopup = new UpdateAvatarPopup(containerPopupContent, avatarContainer, createFormValidator, api);
-  // const userInfo = createUserInfo(api, openEditButton, avatarContainer);
-
-
-  // //main
-
-  // api.getUserInfo()
-  //   .then(res => {
-
-  //     userInfo.setUserInfo(res.name, res.about, res._id, res.avatar);
-  //     userInfo.updateUserInfo();
-
-  //   })
-  //   .catch(err => {
-  //     console.log('Error: ', err);
-  //   })
-
-
-  // getInitialCards();
-
-  // openAddCardButton.addEventListener('click', placeForm._open);
-  // openEditButton.addEventListener('click', editProfileForm._open);
-  // avatarContainer.addEventListener('click', updateAvatarPopup._open);
-
+  signInButton.addEventListener('click', function() {
+    popupSignIn.classList.remove('hidden');
+    popupSignUp.classList.add('hidden');
+    const closePopupButton = popupCloseButtons.namedItem('signin-close');
+    closePopupButton.classList.remove('hidden');
+    addListenerToCloseButton(closePopupButton);
+  });
 })();
