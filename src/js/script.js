06387882617f8628bb37daf0daac631f264d9ebd
ai-small <aismall@yandex.ru>
2020-11-21 17:46:13 +0500

    added fixes for popups, edited head

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line wrap-iife
 (function () {
   const body = document.body;
-  const openMobileMenu = document.querySelector('.button__icon_burger');
+  const openMobileMenuButton = document.querySelector('.button__icon_burger');
   const closeMobileMenuButton = document.querySelector('.button__icon_close');
   const authButton = document.querySelector('.navigation__button');
 
@@ -15,50 +15,57 @@
   const signUpButton = document.getElementById('open-signup');
   const signInButton = document.getElementById('open-signin');
 
+  // закрыть мобильное меню
   function closeMobileMenu() {
     if (!navigation.classList.contains('hidden')) {
       navigation.classList.add('hidden');
       navigation.classList.remove('navigation_flex');
       headerPanel.classList.remove('header__panel_theme_dark');
       overlay.classList.add('hidden');
-      openMobileMenu.classList.remove('hidden');
+      openMobileMenuButton.classList.remove('hidden');
       closeMobileMenuButton.classList.add('hidden');
-      console.log('close burger!');
+    }
+  }
+
+  // открыть мобильное меню: добавляем темную тему в панель хедера,
+  // затемняем страницу, скрываем иконку бургера, показываем кнопку Х
+  function openMobileMenu() {
+    if (navigation.classList.contains('hidden')) {
+      navigation.classList.remove('hidden');
+      navigation.classList.add('navigation_flex');
+      headerPanel.classList.add('header__panel_theme_dark');
+      overlay.classList.remove('hidden');
+      openMobileMenuButton.classList.add('hidden');
+      closeMobileMenuButton.classList.remove('hidden');
     }
   }
 
   function addListenerToCloseButton(closePopupButton) {
     closePopupButton.addEventListener('click', function() {
       closePopupButton.closest('.popup').classList.add('hidden');
-      openMobileMenu.classList.remove('hidden');
+      openMobileMenuButton.classList.remove('hidden');
       body.classList.remove('overflow-hidden');
+      overlay.classList.add('hidden');
     });
   }
 
-  // eslint-disable-next-line prefer-arrow-callback
-  openMobileMenu.addEventListener('click', function() {
-    if (navigation.classList.contains('hidden')) {
-      navigation.classList.remove('hidden');
-      navigation.classList.add('navigation_flex');
-      headerPanel.classList.add('header__panel_theme_dark');
-      overlay.classList.remove('hidden');
-      openMobileMenu.classList.add('hidden');
-      closeMobileMenuButton.classList.remove('hidden');
-      console.log('remove hidden, set theme_dark, set overlay to page');
-    }
-  });
-
-  closeMobileMenuButton.addEventListener('click', closeMobileMenu);
-
-  authButton.addEventListener('click', function() {
+  function openAuthPopup() {
     const closePopupButton = popupCloseButtons.namedItem('signin-close');
     closeMobileMenu();
-    openMobileMenu.classList.add('hidden');
+    openMobileMenuButton.classList.add('hidden');
     popupSignIn.classList.remove('hidden');
-    body.classList.add('overflow-hidden');
+    overlay.classList.remove('hidden');
 
     addListenerToCloseButton(closePopupButton);
-  });
+  }
+
+  //listeners
+
+
+  // eslint-disable-next-line prefer-arrow-callback
+  openMobileMenuButton.addEventListener('click', openMobileMenu);
+  closeMobileMenuButton.addEventListener('click', closeMobileMenu);
+  authButton.addEventListener('click', openAuthPopup);
 
   signUpButton.addEventListener('click', function() {
     popupSignIn.classList.add('hidden');
